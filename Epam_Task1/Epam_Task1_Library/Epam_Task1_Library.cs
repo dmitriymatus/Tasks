655e6697ac5366e290debde066b64b84750f77bd
dmitriymatus <dmitriy.matus@mail.ru>
2015-07-10 18:06:07 +0300

    Add

---
+++ Epam_Task1/Epam_Task1_Library/Epam_Task1_Library.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Epam_Task1
+{
+   /// <summary>
+   /// Класс содержит метод для преобразования строки
+   /// </summary>
+   public class MyConvert
+   {
+      /// <summary>
+      /// Функция преобразования
+      /// </summary>
+      /// <param name="inputString">Строка которую нужно преобразовать</param>
+      /// <returns>Возвращает преобразованную строку</returns>
+      public static string ConvertFunc(string inputString)
+      {
+         StringBuilder result = new StringBuilder();
+         MatchCollection templateStrings = Regex.Matches(inputString, @"\d+.\d+,\d+.\d+");
+         foreach (Match templateString in templateStrings)
+         {
+            Match templateLine = Regex.Match(templateString.Value, @"\d+.\d+");
+            Regex.Replace(templateLine.Value, ".", ",");
+            result.Append("x=");
+            result.AppendLine(templateLine.Value);
+            templateLine = templateLine.NextMatch();
+            Regex.Replace(templateLine.Value, ".", ",");
+            result.Append("y=");
+            result.AppendLine(templateLine.Value);
+         }
+         return result.ToString();
+      }
+      /// <summary>
+      /// Функция преобразования
+      /// </summary>
+      /// <param name="inputFile">Файл который нужно преобразовать</param>
+      /// <returns>Возвращает строку</returns>
+      public static string ConvertFunc(FileStream inputFile)
+      {
+         StreamReader c = new StreamReader(inputFile);
+         string x = c.ReadToEnd();
+         return ConvertFunc(x);
+      }
+   }
+}
---
+++ Epam_Task1/Epam_Task1_WpfApplication/Epam_Task1_WpfApplication.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Epam_Task1;
+namespace EPAM_Task1_WpfApplication
+{
+   /// <summary>
+   /// Логика взаимодействия для MainWindow.xaml
+   /// </summary>
+   public partial class MainWindow : Window
+   {
+      public MainWindow()
+      {
+         InitializeComponent();
+      }
+
+      private void Button_Click(object sender, RoutedEventArgs e)
+      {
+         TextRange inputText = new TextRange(RichTextBox1.Document.ContentStart, RichTextBox1.Document.ContentEnd);
+         string inputString = inputText.Text;
+         TextBox1.Text = (MyConvert.ConvertFunc(inputString));
+      }
+   }
+}
